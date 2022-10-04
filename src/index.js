2625837c08b8f63a5a08d01a33d1528783e01ef4
D1nast <108358234+D1nast@users.noreply.github.com>
2022-10-04 22:16:52 +0900

    add

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ const onClickAdd = () => {
   //入力欄をクリア
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
-
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
   //li生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  //divの子要素にli
+
+  //完了ボタン
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  //削除ボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  //divの子要素を作成
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   //divを子要素にする
   document.getElementById("incomplete-list").appendChild(div);
 };
@@ -22,4 +30,4 @@ const onClickAdd = () => {
 //addEventListener(種類, 関数, false);
 document
   .getElementById("add-button")
-  .addEventListener("click", () => onClickAdd);
+  .addEventListener("click", () => onClickAdd());
