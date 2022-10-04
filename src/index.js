003bdd9417a6e67a0128b4618ec6e1e2f4551b64
D1nast <108358234+D1nast@users.noreply.github.com>
2022-10-04 21:40:22 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  //入力欄をクリア
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
   alert(inputText);
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //divの子要素にli
+  div.appendChild(li);
+  //divを子要素にする
+  document.getElementById("incomplete-list").appendChild(div);
 };
 //HTMLのIDを取得する
 //様々なユーザーからのクリックに対して実行する機能
