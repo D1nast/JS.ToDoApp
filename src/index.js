8932f0f4d69e41b101b09a259ee2d42f5d2e31c8
D1nast <108358234+D1nast@users.noreply.github.com>
2022-10-05 23:34:39 +0900

    add

--- src/index.js
+++ src/index.js
@@ -2,22 +2,50 @@ const onClickAdd = () => {
   //入力欄をクリア
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
+  createIncompleteList(inputText);
+};
+
+//未完了リストから完了リストへ
+const deleteFromIncompletelist = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
   //li生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   //完了ボタン
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
+    deleteFromIncompletelist(completeButton.parentNode);
     //完了リストに追加
     const addTarget = completeButton.parentNode;
     const text = addTarget.firstElementChild.innerText;
-    //未完了リストから削除
-    deleteFromIncompletelist(completeButton.parentNode);
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      //未完了リストに追加
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+    //子要素に追加
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
   //削除ボタン
   const deleteButton = document.createElement("button");
@@ -32,12 +60,6 @@ const onClickAdd = () => {
   //divを子要素にする
   document.getElementById("incomplete-list").appendChild(div);
 };
-
-//未完了リストから完了リストへ
-const deleteFromIncompletelist = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-};
-
 //HTMLのIDを取得する
 //様々なユーザーからのクリックに対して実行する機能
 //addEventListener(種類, 関数, false);
