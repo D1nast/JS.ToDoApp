24f8c6a3f6296cdcb4f5fea740ac196da97ffed7
D1nast <108358234+D1nast@users.noreply.github.com>
2022-10-05 23:02:41 +0900

    add

--- src/index.js
+++ src/index.js
@@ -13,14 +13,17 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //完了リストに追加
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    //未完了リストから削除
+    deleteFromIncompletelist(completeButton.parentNode);
   });
   //削除ボタン
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompletelist(deleteButton.parentNode);
   });
   //divの子要素を作成
   div.appendChild(li);
@@ -29,6 +32,12 @@ const onClickAdd = () => {
   //divを子要素にする
   document.getElementById("incomplete-list").appendChild(div);
 };
+
+//未完了リストから完了リストへ
+const deleteFromIncompletelist = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
 //HTMLのIDを取得する
 //様々なユーザーからのクリックに対して実行する機能
 //addEventListener(種類, 関数, false);
